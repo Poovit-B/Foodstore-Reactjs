f3593555bea7c3e130924b8c69dd6073851c7da0
Poovit-B <61401085+Poovit-B@users.noreply.github.com>
2020-09-17 21:59:55 +0700

    Create component

--- myworkshop/src/components/monitor/Calculator.js
+++ myworkshop/src/components/monitor/Calculator.js
@@ -1,22 +1,27 @@
 import React, {Component} from "react";
 
-
 class Calculator extends Component{
+    // showOrders(orders){
+    //     if(!orders || orders.lenght == 0){
+    //         return <li class="list-group-item"> ไม่มีสินค้า </li>
+    //     }else{
+    //         return orders.map(order => {
+    //             <li class="list-group-item">
+    //                 {order.product.productName} x {order.quantity} = {order.product.unitPrice * order.quantity} ฿
+    //                 <button className="badge badge-danger badge-pill">ลบ</button>
+    //             </li>
+    //         })
+    //     }
+    // }
+    
     render(){
+        const {totalPrice,orders} = this.props;
         return(
             <div >
                 <h4>รายการ</h4>
                 <hr/>
                 <ul class="list-group">
-                <li class="list-group-item">Cras justo odio x 1 = 250 ฿ 
-                    <button className="badge badge-danger badge-pill">ลบ</button>
-                </li>
-                <li class="list-group-item">Cras justo odio x 1 = 250 ฿ 
-                    <button className="badge badge-danger badge-pill">ลบ</button>
-                </li>
-                <li class="list-group-item">Cras justo odio x 1 = 250 ฿ 
-                    <button className="badge badge-danger badge-pill">ลบ</button>
-                </li>
+                    
                 </ul>
                 <h4 className="ml-2 mt-3 text-right">ราคารวม 340 ฿</h4>
                 <hr/>
--- myworkshop/src/components/monitor/Monitor.js
+++ myworkshop/src/components/monitor/Monitor.js
@@ -3,6 +3,10 @@ import Calculator from './Calculator';
 import Productlist from '../product/Productlist';
 
 class Monitor extends Component{
+    constructor(props){
+        super(props);
+        this.setState = {totalPrice:0,orders:[]};
+    }
     render(){
         return(
             <div className="container mt-5">
@@ -11,7 +15,7 @@ class Monitor extends Component{
                         <Productlist products={this.props.products} />
                     </div>
                     <div className="col-md-4">
-                        <Calculator />
+                        <Calculator totalPrice={this.state.totalPrice} orders={this.state.orders} />
                     </div>
                 </div>
             </div>

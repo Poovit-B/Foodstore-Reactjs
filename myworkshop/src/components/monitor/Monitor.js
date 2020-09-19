41dfe0453709c4b77349d0622d2cfe27e110c75c
Poovit-B <61401085+Poovit-B@users.noreply.github.com>
2020-09-19 14:20:09 +0700

    Axios call API methed GET

--- myworkshop/src/components/monitor/Monitor.js
+++ myworkshop/src/components/monitor/Monitor.js
@@ -2,43 +2,43 @@ import React, { Component } from "react";
 import Calculator from './Calculator';
 import Productlist from '../product/Productlist';
 
-class Monitor extends Component{
-    constructor(props){
+class Monitor extends Component {
+    constructor(props) {
         super(props);
-        this.state = {totalPrice:0,orders:[]};
+        this.state = { totalPrice: 0, orders: [] };
         this.addOrder = this.addOrder.bind(this);
         this.delOrder = this.delOrder.bind(this);
     }
 
-    addOrder(product){  
+    addOrder(product) {
 
         let findOrder = this.state.orders.find(order => order.product.productId == product.productId)
-        if(findOrder){
+        if (findOrder) {
             findOrder.quantity++;
-        }else{
-            this.state.orders.push({product : product, quantity : 1})
+        } else {
+            this.state.orders.push({ product: product, quantity: 1 })
         }
         const totalPrice = this.state.totalPrice + parseInt(product.unitPrice);
-        this.setState({totalPrice: totalPrice, orders:this.state.orders});
-    } 
-    delOrder(product){  
+        this.setState({ totalPrice: totalPrice, orders: this.state.orders });
+    }
+    delOrder(product) {
         console.log(product.product.productId)
         let findOrder = this.state.orders.find(order => order.product.productId == product.product.productId)
         let resultOrder = this.state.orders.filter(order => order.product.productId != product.product.productId)
         const totalPrice = this.state.totalPrice - (findOrder.quantity * parseInt(findOrder.product.unitPrice))
-        this.setState({totalPrice: totalPrice, orders:resultOrder});
-    } 
+        this.setState({ totalPrice: totalPrice, orders: resultOrder });
+    }
 
-    render(){ 
-        return(
+    render() {
+        return (
             <div className="container mt-5">
-                <div className="row"> 
+                <div className="row">
                     <div className="col-md-9">
-                        <Productlist products={this.props.products} onAddOrder={this.addOrder}  />
+                        <Productlist products={this.props.products} onAddOrder={this.addOrder} />
                     </div>
                     <div className="col-md-3">
-                        <Calculator totalPrice={this.state.totalPrice} orders={this.state.orders} onDelOrder={this.delOrder}/>
-                    </div> 
+                        <Calculator totalPrice={this.state.totalPrice} orders={this.state.orders} onDelOrder={this.delOrder} />
+                    </div>
                 </div>
             </div>
         )
